e73e12f7dfa818b86963b98d87a99df714e8c593
DreamingDevs <rami.ramilu@gmail.com>
2025-08-22 19:20:38 +0530

    Updated SQL Connectionstring to point to local mac database

--- config.py
+++ config.py
@@ -5,10 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    DB_SERVER = os.getenv("DB_SERVER")
+    DB_SERVER = os.getenv("DB_SERVER")  # e.g. myserver (no instance name)
     DB_NAME = os.getenv("DB_NAME")
-    DB_DRIVER = os.getenv("DB_DRIVER")
+    DB_DRIVER = os.getenv("DB_DRIVER", "ODBC Driver 18 for SQL Server")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
 
     @staticmethod
     def get_connection_string():
-        return f"DRIVER={{{Config.DB_DRIVER}}};SERVER={Config.DB_SERVER};DATABASE={Config.DB_NAME};Trusted_Connection=yes;TrustServerCertificate=yes;"
+        server = f"tcp:{Config.DB_SERVER},1433"
+        return f"DRIVER={{{Config.DB_DRIVER}}};SERVER={server};DATABASE={Config.DB_NAME};Encrypt=yes;TrustServerCertificate=yes;Connection Timeout=30;UID={Config.DB_USER};PWD={Config.DB_PASSWORD};"
